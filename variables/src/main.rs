fb2c056b2be09c9108655c9a8b7dde1b16acceeb
Arup20MA20010 <baralarup1430@gmail.com>
2024-12-11 11:44:03 +0530

    Changes

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,4 +5,16 @@ fn main() {
     println!("After mutating {a}");
     let a = 6;
     println!("After Shadowing {a}");
+    let some_char = '📀';
+    println!("{some_char}");
+    // compound types in rust
+    let tup = (1.2,2,3);
+    let (x,y,z) = tup;
+    println!("x: {x},y: {y}, z:{z}");
+
+    // array 
+    let p = [1,2,3];
+    let q = [3;5]; // first is the element second is the length to get 5 3's
+    println!("{:?}",p);
+    println!("{:?}",q);
 }
